b50da53107662cba1b26705d27bd9504286e5c9d
Mapache <mapache@wonderland.xyz>
2026-01-14 16:40:12 -0400

    fix: deploy import issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { Tabs } from './Tabs';
 export { ConnectWalletModal } from './ConnectWalletModal';
-export { ThemeToggle } from './ThemeToggle';
+export { ThemeToggle } from './ui/ThemeToggle';
 export { SecurityWarning } from './SecurityWarning';
 export { Header } from './Header';
 export { TokenBalance } from './TokenBalance';
