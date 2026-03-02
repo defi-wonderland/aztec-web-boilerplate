44cb80ae838ce0b2e59e020455783dc63cee720a
Mapache <mapache@wonderland.xyz>
2026-03-02 12:23:02 -0400

    feat: export WriteContractActionParams type for improved type safety in contract actions

--- src/use-aztec/actions/index.ts
+++ src/use-aztec/actions/index.ts
@@ -1,4 +1,5 @@
 export { writeContract } from './writeContract';
+export type { WriteContractActionParams } from './writeContract';
 export { readContract } from './readContract';
 export type { ReadContractActionParams } from './readContract';
 export { readContracts } from './readContracts';
