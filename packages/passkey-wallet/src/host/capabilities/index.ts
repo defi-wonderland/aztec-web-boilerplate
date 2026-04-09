68167ff9634617ea325ea478f328e300d11a20c8
Mapache <mapache@wonderland.xyz>
2026-04-09 00:54:42 -0400

    feat(passkey-wallet): add capabilities barrel export

---
+++ packages/passkey-wallet/src/host/capabilities/index.ts
@@ -0,0 +1,5 @@
+export { CapabilityGuard } from './CapabilityGuard';
+export type { GuardPayload } from './CapabilityGuard';
+export { matchesPattern, matchesScope } from './PatternMatcher';
+export { transformCapabilities, prettifyFunctionName, abbreviateAddress } from './PermissionDisplay';
+export type { ContractGroup, PermissionDisplayData } from './PermissionDisplay';
